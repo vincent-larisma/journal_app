443389d2c52644dccebe0622c5b3124472babb86
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-10-22 22:25:39 +0800

    add show, delete function in task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,22 @@ class TasksController < ApplicationController
 
     def create
         @category = Category.find(params[:category_id])
-        @task = @category.tasks.create(task_params)
+        @category.tasks.create(task_params)
 
         redirect_to category_path(@category)
+    end
+
+    def show
+        @category = Category.find(params[:id])
+        @task = @category.tasks.find(params[:category_id])
+    end
+
+
+    def destroy
+        @category = Category.find(params[:category_id])
+        @task = @category.tasks.find(params[:id]).destroy
+        redirect_to category_path(@category)
+
     end
 
     private
