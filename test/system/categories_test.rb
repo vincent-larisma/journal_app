0afa91e4b03b429dce0718073ad53e8052e3acc4
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-11-02 10:31:25 +0800

    add draft testing category

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -4,17 +4,18 @@ class CategoriesTest < ApplicationSystemTestCase
   setup do 
     @categories = categories(:one)
     @user = users(:test)
-  end
-
-  test "visit index category" do
     login_as(@user, 'test@test.com')
-    visit root_path
-    assert_selector "h2", text: "Taskly, task for your dai-ly needs"
+
   end
   
+  test "visit index category" do
+    visit categories_url
+    assert_selector "h2", text: "Taskly, task for your dai-ly needs"
+  end
+
   test "creating a new category" do
-    visit root_path
-    click_on "Create New Task Category!"
+    visit root_url
+    click_link "Create New Task Category!"
 
     fill_in "Category Title", with: "Random Title"
     fill_in "Category Detail", with: "Random Detail"
@@ -23,6 +24,36 @@ class CategoriesTest < ApplicationSystemTestCase
     assert_text "Category was successfully created"
     assert_text "Random Title"
 
+  end
+
+  test "showing a category" do
+    visit root_url
+    click_on "-SHOW", match: :first
+
+    assert_text "Tasks"
+    click_on "Home"
+
+    assert_selector "h2", text: "Taskly, task for your dai-ly needs"
+  end
+
+  test "updating a Category" do
+    visit root_url
+    click_on "-EDIT", match: :first
+
+    fill_in "Category title", with: "Random Title"
+    fill_in "category_category_detail", with: "Random Detail"
+    click_on "Submit"
+
+    assert_text "Category was successfully updated"
+    assert_text "Random Title"
+  end
+
+  test "destroy a category" do
+    visit root_url
+    page.accept_confirm do
+      click_on "-DELETE", match: :first
+    end
+    assert_text "Category was successfully deleted"
   end
 
 end
