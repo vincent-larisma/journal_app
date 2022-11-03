31ead2399e1df38ddc05f6d788207e800be787bb
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-11-03 10:30:10 +0800

    add

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_10_23_111207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "categories", force: :cascade do |t|
     t.string "category_title"
